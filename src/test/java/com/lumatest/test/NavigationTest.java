6bf9f9674976bdfd2b5f417ba428920cd0f972d0
Nataliia Oliver <psvnatali@gmail.com>
2024-06-05 12:33:18 -0500

    Add allure annotations for TC-01

--- src/test/java/com/lumatest/test/NavigationTest.java
+++ src/test/java/com/lumatest/test/NavigationTest.java
@@ -3,6 +3,8 @@ package com.lumatest.test;
 import com.lumatest.base.BaseTest;
 import com.lumatest.data.TestData;
 import io.qameta.allure.*;
+import io.qameta.allure.testng.Tag;
+import io.qameta.allure.testng.Tags;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,10 +12,13 @@ import org.testng.annotations.Test;
 public class NavigationTest extends BaseTest {
 
     @Test(description = "TC-01 Open Base URL")
+    @Tag("smoke")
     @Story("Navigation")
     @Severity(SeverityLevel.BLOCKER)
-    @Description("TC-01 Open Base URL")
+    @Description("To verify that navigating to the base URL of the website opens the correct page by checking " +
+            "the URL and the title of the loaded page.")
     @Link(TestData.BASE_URL)
+    @Owner("tester: Nataliia Oliver")
     public void testOpenBaseURL() {
         Allure.step("SetUp expected results");
         final String expectedURL = TestData.BASE_URL + "/";
@@ -37,11 +42,13 @@ public class NavigationTest extends BaseTest {
             dataProvider = "navigationData",
             dataProviderClass = TestData.class
     )
+    @Tags({@Tag("smoke"), @Tag("navigation")})
     @Story("Navigation")
     @Severity(SeverityLevel.CRITICAL)
     @Description("To verify that the top menu navigation on the website functions correctly by ensuring that " +
             "clicking on menu items directs the user to the expected URL with the expected title.")
     @Link(TestData.BASE_URL)
+    @Owner("tester: Nataliia Oliver")
     public void testNavigationMenu(String baseURL, By navbarMenu, String expectedURL, String expectedTitle) {
 
         Allure.step("Open Base URL");
