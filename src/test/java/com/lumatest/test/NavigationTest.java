7d27e1e35d7153f0a5b08337c450bb87a5f0abf0
Nataliia Oliver <psvnatali@gmail.com>
2024-06-04 23:55:10 -0500

    Implement a data provider on the NavBarMenu

--- src/test/java/com/lumatest/test/NavigationTest.java
+++ src/test/java/com/lumatest/test/NavigationTest.java
@@ -1,21 +1,53 @@
 package com.lumatest.test;
 
 import com.lumatest.base.BaseTest;
+import com.lumatest.data.TestData;
+import io.qameta.allure.*;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NavigationTest extends BaseTest {
 
-    @Test
+    @Test(description = "TC-01 Open Base URL")
+    @Story("Navigation")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("TC-01 Open Base URL")
+    @Link(TestData.BASE_URL)
     public void testOpenBaseURL() {
-        final String expectedURL = "https://magento.softwaretestingboard.com/";
-        final String expectedTitle = "Home Page";
+        Allure.step("SetUp expected results");
+        final String expectedURL = TestData.BASE_URL + "/";
+        final String expectedTitle = TestData.BASE_URL_TITLE;
 
-        getDriver().get("https://magento.softwaretestingboard.com");
+        Allure.step("Open BaseURL");
+        getDriver().get(TestData.BASE_URL);
+
+        Allure.step("Collect actualURL, actualTitle");
         final String actualURL = getDriver().getCurrentUrl();
         final String actualTitle = getDriver().getTitle();
 
+        Allure.step("Verify actualURL as expected");
         Assert.assertEquals(actualURL, expectedURL);
+        Allure.step("Verify actualTitle as expected");
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test(dataProvider = "navigationData", dataProviderClass = TestData.class)
+    public void testNavigationMenu(String baseURL, By navbarMenu, String expectedURL, String expectedTitle) {
+
+        Allure.step("Open Base URL");
+        getDriver().get(baseURL);
+
+        Allure.step("Click on " + navbarMenu.toString());
+        getDriver().findElement(navbarMenu).click();
+
+        Allure.step("Collect actualURL, actualTitle");
+        final String actualURL = getDriver().getCurrentUrl();
+        final String actualTitle = getDriver().getTitle();
+
+        Allure.step("Verify actualURL as expected");
+        Assert.assertEquals(actualURL, expectedURL);
+        Allure.step("Verify actualTitle as expected");
         Assert.assertEquals(actualTitle, expectedTitle);
     }
 }
