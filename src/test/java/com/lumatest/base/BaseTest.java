287fb8c0d28ffaec4b6be6cffe5d1d32925c0acd
Nataliia Oliver <psvnatali@gmail.com>
2024-06-19 14:25:35 -0500

    commited edge setup

--- src/test/java/com/lumatest/base/BaseTest.java
+++ src/test/java/com/lumatest/base/BaseTest.java
@@ -18,7 +18,7 @@ public abstract class BaseTest {
     protected void setupWebDriverManager() {
         WebDriverManager.chromedriver().setup();
         WebDriverManager.firefoxdriver().setup();
-        WebDriverManager.edgedriver().setup();
+        //WebDriverManager.edgedriver().setup();
         //WebDriverManager.operadriver().setup();
         //WebDriverManager.chromiumdriver().setup();
     }
